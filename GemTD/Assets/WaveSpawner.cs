cd005f982a0b279f94869d87282648c7cd52363c
Jakob Boisen <jb@vitasim.dk>
2020-01-27 11:45:27 +0100

    Wave Spawner Implemented

---
+++ GemTD/Assets/WaveSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPreFab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = "New Wave In: " + Mathf.Round(countdown).ToString();
+    }
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave Incomming!");
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPreFab, spawnPoint.position, spawnPoint.rotation);
+    }
+}

01cbce7bd4e7cd4a130ffeb714cef496d2663947
Jakob Boisen <jb@vitasim.dk>
2020-01-27 10:56:59 +0100

    Enemy script
    Enemies move to waypoints and die at the end

---
+++ GemTD/Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+    void GetNextWayPoint()
+    {
+        if (waypointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = WayPoints.points[waypointIndex];
+    }
+}
